3c0cad89b75a4d0a8a0c1bf532043e69282e75c3
RyutaMatsuoka <105757895+uriryu@users.noreply.github.com>
2022-09-19 03:41:51 +0000

    [Add]works index [Fixed]public_sign_up

--- app/controllers/admin/works_controller.rb
+++ app/controllers/admin/works_controller.rb
@@ -1,9 +1,12 @@
 class Admin::WorksController < ApplicationController
   before_action :authenticate_admin!
   before_action :ensure_work, only: [:show, :edit, :update, :destroy]
-  
+
   def index
-    @works = Work.all
+    @user = User.find(params[:id])
+    all_works = @user.works
+    @works = all_works
+    @all_works_count = all_works.count
   end
 
   def show
@@ -11,13 +14,13 @@ class Admin::WorksController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def work_params
     params.require(:work).permit(:image, :title, :body, :url)
   end
-  
+
   def ensure_work
     @work = Work.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  
+  has_many :works, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   enum is_editor: { normal_user: 0, editor_user: 1 }
- 
+
  has_one_attached :profile_image
- 
+
  def get_profile_image
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.png')
@@ -18,5 +19,5 @@ class User < ApplicationRecord
   profile_image.variant(resize_to_limit: [width, height]).processed
   # Viewの方で引数を設定するとそのサイズにリサイズできる形になっている↑。
  end
- 
+
 end
